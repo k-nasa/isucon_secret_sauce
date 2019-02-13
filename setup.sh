10b15056c86aae379d0efda3a3c75b6f91f2c94c
nasa <htilcs1115@gmail.com>
2019-02-13 21:14:40 +0900

    install pt-querydigest

--- setup.sh
+++ setup.sh
@@ -16,3 +16,8 @@ echo 'Install neovim'
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
+
+# Install pt-query-digest
+wget https://github.com/percona/percona-toolkit/archive/3.0.5-test.tar.gz
+tar zxvf 3.0.5-test.tar.gz
+./percona-toolkit-3.0.5-test/bin/pt-query-digest --version
