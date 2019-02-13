9eb049adefe322b56bce5ddc736373ebae301375
nasa <htilcs1115@gmail.com>
2019-02-13 21:23:33 +0900

    install pt-...

--- setup.sh
+++ setup.sh
@@ -21,3 +21,5 @@ sudo apt-get install neovim
 wget https://github.com/percona/percona-toolkit/archive/3.0.5-test.tar.gz
 tar zxvf 3.0.5-test.tar.gz
 ./percona-toolkit-3.0.5-test/bin/pt-query-digest --version
+sudo mv ./percona-toolkit-3.0.5-test/bin/pt-query-digest /usr/local/bin/pt-query-digest
+sudo chown root:root /usr/local/bin/alp
