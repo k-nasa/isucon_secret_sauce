99917e95f2e597d6f0d1eb74ef53b2233d7ce638
nasa <htilcs1115@gmail.com>
2019-02-16 11:24:50 +0900

    fix path

--- setup.sh
+++ setup.sh
@@ -12,6 +12,6 @@ wget https://github.com/percona/percona-toolkit/archive/3.0.5-test.tar.gz
 tar zxvf 3.0.5-test.tar.gz
 ./percona-toolkit-3.0.5-test/bin/pt-query-digest --version
 sudo mv ./percona-toolkit-3.0.5-test/bin/pt-query-digest /usr/local/bin/pt-query-digest
-sudo chown root:root /usr/local/bin/alp
+sudo chown root:root /usr/local/bin/pt-query-digest
 
 ./alp_setup.sh
