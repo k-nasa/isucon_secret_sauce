408da3be7a8e6ff2a8f44acdee92cca9d256ef07
nasa <htilcs1115@gmail.com>
2019-02-16 11:24:36 +0900

    alpをまとめる

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 # Install zsh
 sudo apt-get install zsh
 
-# Install apl
-echo 'Install alp'
-wget https://github.com/tkuchiki/alp/releases/download/v0.0.4/alp_linux_amd64.zip
-sudo apt install unzip
-unzip alp_linux_amd64.zip
-sudo mv alp_linux_amd64 alp
-sudo mv alp /usr/local/bin/alp
-# パスの通っているディレクトリにalpをインストール
-sudo chown root:root /usr/local/bin/alp
-
 # Install neovim
 echo 'Install neovim'
 sudo add-apt-repository ppa:neovim-ppa/unstable
@@ -23,3 +13,5 @@ tar zxvf 3.0.5-test.tar.gz
 ./percona-toolkit-3.0.5-test/bin/pt-query-digest --version
 sudo mv ./percona-toolkit-3.0.5-test/bin/pt-query-digest /usr/local/bin/pt-query-digest
 sudo chown root:root /usr/local/bin/alp
+
+./alp_setup.sh
