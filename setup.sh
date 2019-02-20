f8d0c0d64ebc6eb5ea41c9545569a5a0e77d40ff
nasa <htilcs1115@gmail.com>
2019-02-20 19:44:19 +0900

    feat: install netdata

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@
 # Install redis
 sudo apt-get install redis-server
 
+# Install netdata
+bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+echo "sudo systemctl start netdata http://yourIP:19999"
+
 ./alp_setup.sh
 
