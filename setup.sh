3d6783f9f313c0c801896233e47df12e9ba2ce2e
k-nasa <htilcs1115@gmail.com>
2019-06-01 16:30:11 +0900

    Add debug message

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 # setup shell
+echo "setup dev tools"
 ./shell_nvim_setup.sh
 
 # Install pt-query-digest
+echo "install pt-query-digest"
 ./pt_query_digest_setup.sh
 
+echo "install alp"
+./alp_setup.sh
+
 # Install redis
+echo "install redis server"
 sudo apt-get install redis-server
 
 # Install netdata
+echo "install netdata"
 bash <(curl -Ss https://my-netdata.io/kickstart.sh)
-echo "sudo systemctl start netdata http://yourIP:19999"
-
-./alp_setup.sh
-
+sudo systemctl restart netdata
