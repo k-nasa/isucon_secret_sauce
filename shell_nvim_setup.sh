c55a082962b53621481368018b2eb931c7243098
k-nasa <htilcs1115@gmail.com>
2019-06-01 17:06:26 +0900

    fix

--- shell_nvim_setup.sh
+++ shell_nvim_setup.sh
@@ -7,13 +7,17 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
 
+# cent os version
+# sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# sudo yum install -y neovim python{2,3}-neovim
+
 # Setup dev tool
 echo 'setup dev tool'
 mkdir ~/.config/
 
-ln -sf ./dotfile/nvim ~/.config/nvim
-ln -sf ./dotfile/.zshrc ~/
-ln -sf ./dotfile/.zprofile ~/
+ln -sf ~/isucon_secret_sauce/dotfile/nvim ~/.config/nvim
+ln -sf ~/isucon_secret_sauce/dotfile/.zshrc ~/
+ln -sf ~/isucon_secret_sauce/dotfile/.zprofile ~/
 
 chsh -s $(which zsh)
 
