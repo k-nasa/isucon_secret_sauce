d0809093d46a7807ca2757471e89682cbe8c6c6c
k-nasa <htilcs1115@gmail.com>
2019-06-24 13:24:44 +0900

    fix typo

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -5,8 +5,8 @@ sudo apt-get install zsh || echo 'failed install zsh'
 
 echo 'setup dev tool'
 mkdir ~/.config/
-cp ~/isucon_secret_sauce/dotfile/.zshrc ~/
-cp ~/isucon_secret_sauce/dotfile/.zprofile ~/
+cp ~/isucon_secret_sauce/dotfiles/.zshrc ~/
+cp ~/isucon_secret_sauce/dotfiles/.zprofile ~/
 
 chsh -s $(which zsh)
 
