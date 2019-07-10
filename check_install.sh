c16cfa4f234ef8c50c3bcd297b0b3604db417115
k-nasa <htilcs1115@gmail.com>
2019-07-10 09:55:34 +0900

    fix check_install

--- check_install.sh
+++ check_install.sh
@@ -1,48 +1,21 @@
-echo -e '\e[33m
+printf "\e[33m
 ==============================
 install checking!
 ==============================
-'
-which pt-query-digest > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo -e '\e[32m pt-query-digest ok'
-else
-  echo -e "\e[31m pt-query-digest notinstalled!"
-fi
+"
 
-which alp > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo -e '\e[32m alp ok'
-else
-  echo -e "\e[31m alp notinstalled!"
-fi
+checking() {
+  which $1 > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    printf "\e[32m ok $1 \e[m\n"
+  else
+    printf "\e[31m notinstalled $1 \e[m \n"
+  fi
+}
 
-which netdata > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo -e '\e[32m netdata ok'
-else
-  echo -e "\e[31m netdata notinstalled!"
-fi
-
-which zsh > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo -e '\e[32m zsh ok'
-else
-  echo -e "\e[31m zsh notinstalled!"
-fi
-
-which nvim > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo -e '\e[32m neovim ok'
-else
-  echo -e "\e[31m neovim notinstalled!"
-fi
-
-which htop > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo -e '\e[32m htop ok'
-else
-  echo -e "\e[31m htop notinstalled!"
-fi
-
-echo 'finish'
+checking pt-query-digest
+checking alp
+checking netdata
+checking zsh
+checking nvim
+checking htop
