e6635dbaef4a2be5ba1f2235e55d0a74ff420b0b
k-nasa <htilcs1115@gmail.com>
2019-07-10 09:57:42 +0900

    fix check_install

--- check_install.sh
+++ check_install.sh
@@ -1,15 +1,15 @@
 printf "\e[33m
 ==============================
 install checking!
-==============================
-"
+==============================\n
+\e[m"
 
 checking() {
   which $1 > /dev/null 2>&1
   if [ $? -eq 0 ]; then
-    printf "\e[32m ok $1 \e[m\n"
+    printf "\e[32m [ok]   $1 \e[m\n"
   else
-    printf "\e[31m notinstalled $1 \e[m \n"
+    printf "\e[31m [err]  $1 \e[m \n"
   fi
 }
 
