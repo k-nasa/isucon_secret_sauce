b4ef03a4c23a3acb103d2eaf05096ada4f8fda9e
k-nasa <htilcs1115@gmail.com>
2019-07-10 10:03:06 +0900

    fix install zsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -4,11 +4,10 @@ sudo apt-get install zsh || echo 'failed install zsh'
 # sudo yum install zsh || echo 'failed install zsh'
 
 echo 'setup dev tool'
-mkdir ~/.config/
+sudo mkdir ~/.config/
 cp ~/isucon_secret_sauce/dotfiles/.zshrc ~/
 cp ~/isucon_secret_sauce/dotfiles/.zprofile ~/
 
-chsh -s $(which zsh)
-
+zsh
 source ~/.zprofile
 source ~/.zshrc
